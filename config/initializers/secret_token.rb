6e2bed0526e0501832da283573efab9f074fbe47
Todd Mazierski <toddmazierski@gmail.com>
2013-04-06 11:31:48 -0400

    Starting a new Rails 3 application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NyplTweetwall::Application.config.secret_token = 'b5659e0aed07d193840f6a5741ebce01635b7ecd21a5263f83b770683d7c2a58b609d64cd260f4d34061438967aa8d16405cdbc9b7746529a2ffa6227ab84e4f'
