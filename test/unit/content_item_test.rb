ba28b686743017ea8bc7a91a879b8411f030691d
Todd Mazierski <toddmazierski@gmail.com>
2013-09-07 21:37:21 -0400

    Change order of execution, add comments for clarity.

--- test/unit/content_item_test.rb
+++ test/unit/content_item_test.rb
@@ -17,12 +17,12 @@ class ContentItemTest < ActiveSupport::TestCase
   end
 
   def test_exclude_without_tweets!
-    processed_content_items = [
-      blog_content_items(:musical),
+    expected_content_items = [
       blog_content_items(:road_trip),
       blog_content_items(:wonder_years)
     ]
-    expected_content_items = [
+    processed_content_items = [
+      blog_content_items(:musical),
       blog_content_items(:road_trip),
       blog_content_items(:wonder_years)
     ]
@@ -31,14 +31,14 @@ class ContentItemTest < ActiveSupport::TestCase
   end
 
   def test_sort_by_first_tweet!
-    processed_content_items = [
-      blog_content_items(:wonder_years),
-      blog_content_items(:new_york_times),
-      blog_content_items(:road_trip)
-    ]
     expected_content_items = [
       blog_content_items(:new_york_times),
       blog_content_items(:wonder_years),
+      blog_content_items(:road_trip)
+    ]
+    processed_content_items = [
+      blog_content_items(:wonder_years),
+      blog_content_items(:new_york_times),
       blog_content_items(:road_trip)
     ]
     ContentItem.sort_by_first_tweet!(processed_content_items)
@@ -46,15 +46,19 @@ class ContentItemTest < ActiveSupport::TestCase
   end
 
   def test_delete_expired
-    blog_content_items(:wonder_years).update_attribute(:created_at, 5.minutes.ago)
-    blog_content_items(:road_trip).update_attribute(:created_at, 30.minutes.ago)
-    blog_content_items(:new_york_times).update_attribute(:created_at, 2.hours.ago)
-    blog_content_items(:musical).update_attribute(:created_at, 1.week.ago)
-    ContentItem.delete_expired
+    # These content items ought *not* to expire.
     expected_content_items = [
       blog_content_items(:wonder_years),
       blog_content_items(:road_trip),
     ]
+    blog_content_items(:wonder_years).update_attribute(:created_at, 5.minutes.ago)
+    blog_content_items(:road_trip).update_attribute(:created_at, 30.minutes.ago)
+
+    # These content items *ought* to expire.
+    blog_content_items(:new_york_times).update_attribute(:created_at, 2.hours.ago)
+    blog_content_items(:musical).update_attribute(:created_at, 1.week.ago)
+
+    ContentItem.delete_expired
     assert_equal expected_content_items, BlogContentItem.all
   end
 end
